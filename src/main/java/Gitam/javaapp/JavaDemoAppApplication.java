eaf5ee943cf43144f861ad008dcfbe83044ce727
sagar maya <mindmatters0316@gmail.com>
2024-09-07 13:03:47 +0530

    updated java app

---
+++ src/main/java/Gitam/javaapp/JavaDemoAppApplication.java
@@ -0,0 +1,13 @@
+package Gitam.javaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gitam/javaapp/JavaDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Gitam.javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
